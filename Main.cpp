dc562dec53abf42dcf1b9f8df1b5fc831b959b4c
dtanner24 <77747943+dtanner24@users.noreply.github.com>
2022-03-10 14:22:24 -0600

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -9,5 +9,39 @@ int main()
 	float totalheight;
 	float weight;
 	float BMI;
-	cout << "" << endl;
+	cout << "Please enter your height below:" << endl;
+	cout << "Feet: ";
+	cin >> feet;
+	cout << "Inches: ";
+	cin >> inches;
+	cout << endl;
+	cout << "Please enter your weight in pounds: ";
+	cin >> weight;
+	totalheight = (feet * 12) + inches;
+	float BMIweight;
+	float BMIheight;
+	BMIweight = weight * 0.45;
+	BMIheight = totalheight * 0.025;
+	float BMIheight2;
+	BMIheight2 = BMIheight * BMIheight;
+	BMI = BMIweight / BMIheight2;
+	cout << "BMI: " << BMI << endl;
+	if (BMI <= 18.5)
+	{
+		cout << "Underweight" << endl;
+	}
+	if (BMI > 18.5 && BMI <= 24.9)
+	{
+		cout << "Normal Weight" << endl;
+	}
+	if (BMI > 24.9 && BMI <= 29.9)
+	{
+		cout << "Overweight" << endl;
+	}
+	if (BMI > 29.9)
+	{
+		cout << "Obese" << endl;
+	}
+	
+	return 0;
 }
